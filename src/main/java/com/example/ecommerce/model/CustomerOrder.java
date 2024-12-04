e8d70e795712b87b712d8abf5a812c515e7aa317
NguyenMinhJuan <nmtuan.dhti14a4hn@sv.uneti.edu.vn>
2024-12-04 14:22:08 +0700

    sua model CustomerOrder

--- src/main/java/com/example/ecommerce/model/CustomerOrder.java
+++ src/main/java/com/example/ecommerce/model/CustomerOrder.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.model;
 
+import com.example.ecommerce.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -9,7 +10,8 @@ public class CustomerOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
